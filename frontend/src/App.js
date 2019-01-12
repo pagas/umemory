b5571b40aaf2e6cdc35248a8440169422744a88b
paulius <paulius@2simple.com>
2019-01-12 22:57:41 +0000

    add question component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
+
 import Navbar from './navbar/navbar';
 import Questions from './questions/questions';
+import Question from './question/question';
 
 class App extends Component {
   render() {
     return (
         <div>
             <Navbar/>
-            <Questions/>
+            <Route exact path='/' component={Questions}/>
+            <Route exact path='/question/:questionId' component={Question}/>
         </div>
     );
   }
